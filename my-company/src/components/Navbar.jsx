fab4715cae805c3b7ac82ffab965407e0c7ec9c9
Vieve-e <vivearonu@gmail.com>
2024-11-09 23:09:15 +0100

    nav bar updated

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,20 +4,13 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
+
+    <nav style={{ backgroundColor: '#282c34', padding: '20px', justifyContent: 'center', color: 'white' }}>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li> 
       </ul>
     </nav>
   );
