832b8eb7fb818ceb6ac1c691daf04d0f075c7392
Vieve-e <vivearonu@gmail.com>
2024-11-24 18:49:28 +0100

    TodoList

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Buy milk', completed: false },
+    { id: 2, text: 'Walk the dog', completed: false },
+    { id: 3, text: 'Do laundry', completed: false },
+  ]);
+
+  const handleDelete = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const handleToggle = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleAdd = (text) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random(), text, completed: false },
+    ]);
+  };
+
   return (
     <div>
       <h1>Todo App</h1>
-      <TodoList />
+      <TodoForm onAdd={handleAdd} />
+      <TodoList todos={todos} onDelete={handleDelete} onToggle={handleToggle} />
     </div>
   );
-}
-
+};
 export default App;
