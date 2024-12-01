38a3334b0ba65c8b72d3f95c6b0f0887115eea10
Vieve-e <vivearonu@gmail.com>
2024-12-01 18:04:21 +0100

    Tailwind

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+function UserProfile() {
+    return (
+      <div className= "bg-gray-100 sm:p-4 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+        <img class="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto" src="https://via.placeholder.com/150" alt="User" />
+        <h1 class="sm:text-lg md:text-xl text-blue-800 my-4 ">John Doe</h1>
+        <p class= "text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      </div>
+    );
+  }
+  
+  export default UserProfile;
